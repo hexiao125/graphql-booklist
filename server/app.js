4acd762d20e14f998e3126b3e469692d663d5082
Xiao <hexiao125@gmail.com>
2019-04-12 23:11:58 +0200

    Set up MongoDB Atlas and mongoose

--- server/app.js
+++ server/app.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+
+//connect to mongoDB
+const URL_DB = 'mongodb+srv://Xiao:test123@cluster0-qrpds.mongodb.net/test?retryWrites=true';
+mongoose.connect(URL_DB, {useNewUrlParser: true});
+
+mongoose.connection.once('open', () => {
+    console.log('connected to database');
+})
 
 //bind express with graphql
 app.use('/graphql', graphqlHTTP({
