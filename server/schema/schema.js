8749c0c563444d65bf61635eb82058bd635575cf
Xiao <hexiao125@gmail.com>
2019-04-12 23:34:33 +0200

    Added mongoose schema

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,9 +1,11 @@
 const graphql = require('graphql');
 const _ = require('lodash');
+const Book = require('../models/book');
+const Author = require('../models/author');
 
-const { 
-    GraphQLObjectType, 
-    GraphQLString, 
+const {
+    GraphQLObjectType,
+    GraphQLString,
     GraphQLSchema,
     GraphQLID,
     GraphQLInt,
@@ -11,32 +13,32 @@ const {
 } = graphql;
 
 //dummy data 
-var books = [
+/* var books = [
     { name: 'Name of the Wind', genre: 'Fantasy', id: '1', authorId: '1' },
     { name: 'The Final Empire', genre: 'Fantasy', id: '2', authorId: '2' },
-    { name: 'The Hero of Ages', genre: 'Fantasy', id: '4', authorId: '2'},
-    { name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3'},
-    { name: 'The Colour of Magic', genre: 'Fantasy', id: '5', authorId: '3'},
-    { name: 'The Light Fantastic', genre: 'Fantasy', id: '6', authorId: '3'}
+    { name: 'The Hero of Ages', genre: 'Fantasy', id: '4', authorId: '2' },
+    { name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3' },
+    { name: 'The Colour of Magic', genre: 'Fantasy', id: '5', authorId: '3' },
+    { name: 'The Light Fantastic', genre: 'Fantasy', id: '6', authorId: '3' }
 ];
 
 var authors = [
     { name: 'Patrick Rothfuss', age: 44, id: '1' },
     { name: 'Brandon Sanderson', age: 42, id: '2' },
-    { name: 'Terry Prachett', age: 66, id: '3'}
-];
+    { name: 'Terry Prachett', age: 66, id: '3' }
+]; */
 
 const BookType = new GraphQLObjectType({
     name: 'Book',
     fields: () => ({
-        id: {type: GraphQLID},
-        name: {type: GraphQLString},
-        genre: {type: GraphQLString},
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        genre: { type: GraphQLString },
         author: {
             type: AuthorType,
-            resolve(parent, args){
-               // console.log(parent);
-                return _.find(authors, {id: parent.authorId});
+            resolve(parent, args) {
+                // console.log(parent);
+                //return _.find(authors, { id: parent.authorId });
             }
         }
     })
@@ -45,13 +47,13 @@ const BookType = new GraphQLObjectType({
 const AuthorType = new GraphQLObjectType({
     name: 'Author',
     fields: () => ({
-        id: {type: GraphQLID},
-        name: {type: GraphQLString},
-        age: {type: GraphQLInt},
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        age: { type: GraphQLInt },
         books: {
             type: new GraphQLList(BookType),
-            resolve(parent, args){
-                return _.filter(books, {authorId: parent.id});
+            resolve(parent, args) {
+               // return _.filter(books, { authorId: parent.id });
             }
         }
     })
@@ -63,27 +65,27 @@ const RootQuery = new GraphQLObjectType({
         book: {
             type: BookType,
             args: { id: { type: GraphQLID } },
-            resolve(parent, args){
-                return _.find(books, { id: args.id });
+            resolve(parent, args) {
+                //return _.find(books, { id: args.id });
             }
         },
         author: {
             type: AuthorType,
-            args: {id: { type: GraphQLID }},
-            resolve(parent, args){
-                return _.find(authors, {id: args.id});
+            args: { id: { type: GraphQLID } },
+            resolve(parent, args) {
+                //return _.find(authors, { id: args.id });
             }
         },
         books: {
             type: new GraphQLList(BookType),
-            resolve(parent, args){
-                return books;
+            resolve(parent, args) {
+                //return books;
             }
         },
         authors: {
             type: new GraphQLList(AuthorType),
-            resolve(parent, args){
-                return authors;
+            resolve(parent, args) {
+                //return authors;
             }
         }
     }
