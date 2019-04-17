d10373f46cc7e95d6076eb92d6eee111cb8c5d7b
Xiao <hexiao125@gmail.com>
2019-04-17 20:42:11 +0200

    Updated resolve functions

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -39,6 +39,7 @@ const BookType = new GraphQLObjectType({
             resolve(parent, args) {
                 // console.log(parent);
                 //return _.find(authors, { id: parent.authorId });
+                return Author.findById(parent.authorId);
             }
         }
     })
@@ -54,6 +55,7 @@ const AuthorType = new GraphQLObjectType({
             type: new GraphQLList(BookType),
             resolve(parent, args) {
                 // return _.filter(books, { authorId: parent.id });
+                return Book.find({authorId: parent.id});
             }
         }
     })
@@ -67,6 +69,7 @@ const RootQuery = new GraphQLObjectType({
             args: { id: { type: GraphQLID } },
             resolve(parent, args) {
                 //return _.find(books, { id: args.id });
+                return Book.findById(args.id);
             }
         },
         author: {
@@ -74,18 +77,21 @@ const RootQuery = new GraphQLObjectType({
             args: { id: { type: GraphQLID } },
             resolve(parent, args) {
                 //return _.find(authors, { id: args.id });
+                return Author.findById(args.id);
             }
         },
         books: {
             type: new GraphQLList(BookType),
             resolve(parent, args) {
-                //return books;
+                //return all books
+                return Book.find({});
             }
         },
         authors: {
             type: new GraphQLList(AuthorType),
             resolve(parent, args) {
-                //return authors;
+                //return all authors;
+                return Author.find({});
             }
         }
     }
