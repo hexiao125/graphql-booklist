842828ca946b8d7fc353e493f02f3ccf6a6eae95
Xiao <hexiao125@gmail.com>
2019-05-15 18:04:10 +0200

    Add CSS to the app

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -29,7 +29,7 @@ class BookList extends Component {
   render() {
     return (
       <div>
-        <ul id="book-list">{this.displayBooks()}</ul>
+        <ul className="book-list">{this.displayBooks()}</ul>
         <BookDetails bookId={this.state.selected} />
       </div>
     );
