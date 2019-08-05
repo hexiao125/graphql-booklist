50941f92122f9ffe92e40ea1e56b101aabfb926c
Xiao <hexiao125@gmail.com>
2019-08-05 10:35:39 +0200

    Set environment variable

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const graphqlHTTP = require('express-graphql');
-const schema = require('./schema/schema');
-const mongoose = require('mongoose');
-const cors = require('cors');
+const express = require("express");
+const graphqlHTTP = require("express-graphql");
+const schema = require("./schema/schema");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
 
@@ -10,19 +10,24 @@ const app = express();
 app.use(cors());
 
 //connect to mongoDB
-const URL_DB = 'mongodb+srv://Xiao:test123@cluster0-qrpds.mongodb.net/test?retryWrites=true';
-mongoose.connect(URL_DB, {useNewUrlParser: true});
+const URL_DB =
+  "mongodb+srv://Xiao:test123@cluster0-qrpds.mongodb.net/test?retryWrites=true";
+mongoose.connect(URL_DB, { useNewUrlParser: true });
 
-mongoose.connection.once('open', () => {
-    console.log('connected to database');
-})
+mongoose.connection.once("open", () => {
+  console.log("connected to database");
+});
 
 //bind express with graphql
-app.use('/graphql', graphqlHTTP({
+app.use(
+  "/graphql",
+  graphqlHTTP({
     schema,
     graphiql: true
-}));
+  })
+);
 
-app.listen(4000, () => {
-    console.log('now listeing for requests on port 4000');
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+  console.log("now listeing for requests on port 4000");
 });
